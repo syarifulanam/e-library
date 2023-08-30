fee65e946b6a81a394ccc85ba75452b5e7036fea
syarifulanam <em.syariful.anam@gmail.com>
2023-08-30 20:04:49 +0700

    menambahkan circulations, books and members

---
+++ src/main/java/com/anam/elibrary/controller/MembersController.java
@@ -0,0 +1,28 @@
+package com.anam.elibrary.controller;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MembersController {
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        model.addAttribute("titlePage", "MEMBERS");
+        return "dashboard/members";
+    }
+
+    @GetMapping("/members/create")
+    public String membersCreate(Model model) {
+        model.addAttribute("titlePage", "MEMBERS CREATE");
+        return "dashboard/members-create";
+    }
+
+    @GetMapping("/members/edit")
+    public String membersEdit(Model model) {
+        model.addAttribute("titlePage", "MEMBERS EDIT");
+        return "dashboard/members-edit";
+    }
+}
