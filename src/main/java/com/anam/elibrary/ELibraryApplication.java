1f8a11022d95e1968c0edc887ea44f926fa777af
syarifulanam <em.syariful.anam@gmail.com>
2023-09-07 19:18:56 +0700

    adding TemplateResolver

--- src/main/java/com/anam/elibrary/ELibraryApplication.java
+++ src/main/java/com/anam/elibrary/ELibraryApplication.java
@@ -1,13 +1,27 @@
 package com.anam.elibrary;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class ELibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ELibraryApplication.class, args);
+	}
+
+	@Bean
+	public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+		ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+		secondaryTemplateResolver.setPrefix("templates");
+		secondaryTemplateResolver.setSuffix(".html");
+		secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+		secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+		secondaryTemplateResolver.setOrder(1);
+		secondaryTemplateResolver.setCheckExistence(true);
+		return secondaryTemplateResolver;
 	}
 }
